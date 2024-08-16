2789687dc080865e28f9a51a274c6bba51f1d583
bepaald <38437099+bepaald@users.noreply.github.com>
2024-08-16 07:01:53 +0000

    Initial release

---
+++ getsecret_mac.cc
@@ -0,0 +1,105 @@
+#include <Security/Security.h>
+
+#include "main.h"
+#include <iostream>
+
+/*
+
+  security dump-keychain:
+
+keychain: "/Users/bastimmer/Library/Keychains/login.keychain-db"
+version: 512
+class: "genp"
+attributes:
+    0x00000007 <blob>="Signal Safe Storage"
+    0x00000008 <blob>=<NULL>
+    "acct"<blob>="Signal Key"
+    "cdat"<timedate>=0x32303234303831353134333230395A00  "20240815143209Z\000"
+    "crtr"<uint32>="aapl"
+    "cusi"<sint32>=<NULL>
+    "desc"<blob>=<NULL>
+    "gena"<blob>=<NULL>
+    "icmt"<blob>=<NULL>
+    "invi"<sint32>=<NULL>
+    "mdat"<timedate>=0x32303234303831353134333230395A00  "20240815143209Z\000"
+    "nega"<sint32>=<NULL>
+    "prot"<blob>=<NULL>
+    "scrp"<sint32>=<NULL>
+    "svce"<blob>="Signal Safe Storage"
+    "type"<uint32>=<NULL>
+
+
+
+    mqqCXCU/iaszlgACa4aWZg==
+
+
+{
+  "encryptedKey": "763130345418473a12fbfd3a7fa4aa6baa7c4fe9069b2b09f28a4caf1dfcd280643333064839e596d18167225eba0ecd0c6245050a5e2ff73fd7319087443faca8e72debf94b70dd6732b5e979dc7cfd8f5318"
+}
+
+*/
+
+char *CFStringCopyUTF8String(CFStringRef cfsr)
+{
+  if (!cfsr)
+    return nullptr;
+
+  CFIndex length = CFStringGetLength(cfsr);
+  CFIndex max_length = CFStringGetMaximumSizeForEncoding(length, kCFStringEncodingUTF8) + 1;
+  char *buffer = new char[max_length];
+  if (CFStringGetCString(cfsr, buffer, max_length, kCFStringEncodingUTF8))
+    return buffer;
+  delete[] buffer;
+  return nullptr;
+}
+
+void getSecret_Mac(std::set<std::string> *secrets)
+{
+  int const dict_size = 4;
+  void const *keys[dict_size] = {kSecClass,
+				 kSecAttrAccount,
+				 kSecAttrService,
+				 kSecReturnData};
+  CFStringRef account = CFStringCreateWithCString(nullptr, "Signal Key", kCFStringEncodingUTF8);
+  CFStringRef service = CFStringCreateWithCString(nullptr, "Signal Safe Storage", kCFStringEncodingUTF8);
+  void const *values[dict_size] = {kSecClassGenericPassword,
+				   account,
+				   service,
+				   kCFBooleanTrue};
+  CFDictionaryRef query = CFDictionaryCreate(nullptr, keys, values, dict_size, nullptr, nullptr);
+
+  CFDataRef item_data = nullptr;
+  OSStatus ret = SecItemCopyMatching(query, reinterpret_cast<CFTypeRef *>(&item_data));
+
+  // release some refs...
+  CFRelease(query);
+  CFRelease(account);
+  CFRelease(service);
+
+  if (ret != 0)
+  {
+    std::cout << ret << std::endl;
+    char *str = CFStringCopyUTF8String(SecCopyErrorMessageString(ret, nullptr));
+    if (str)
+    {
+      std::cout << str << std::endl;
+      delete[] str;
+    }
+    return;
+  }
+
+  // parse returned items...
+  int password_length = CFDataGetLength(item_data);
+  if (password_length != 24)
+  {
+    std::cout << "Unexpected password length" << std::endl;
+    return;
+  }
+  std::string password(reinterpret_cast<char const *>(CFDataGetBytePtr(item_data)), password_length);
+
+  if (g_verbose) [[unlikely]] std::cout << "Got password: " << password << std::endl;
+
+  secrets->emplace(password);
+
+  return;
+}
---
+++ main.cc
@@ -0,0 +1,88 @@
+/*
+  Copyright (C) 2024  Selwin van Dijk
+
+  This file is part of get_signal_desktop_key.
+
+  get_signal_desktop_key is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  get_signal_desktop_key is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with get_signal_desktop_key.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <iostream>
+#include <cstdlib>
+
+#include "main.h"
+
+bool g_verbose;
+
+int main(int argc, char *argv[])
+{
+  auto getKey = [](std::set<std::string> const &secrets, std::string const &encryptedkey, std::string &decrypted)
+  {
+    if (g_verbose) [[unlikely]]
+      for (auto const &s : secrets)
+        std::cout << "(Got secrets: " << s << ")" << std::endl;
+    for (auto const &s : secrets)
+    {
+      decrypted = decryptKey_linux_mac(s, encryptedkey);
+      if (!decrypted.empty())
+        return true;
+    }
+    return false;
+  };
+
+  // arg handling
+  g_verbose = false;
+  std::string signal_config_file(std::getenv("HOME"));
+  signal_config_file += "/Library/Application Support/Signal/config.json";
+  for (int i = 1; i < argc; ++i)
+  {
+    if (argv[i] == "-v"s)
+      g_verbose = true;
+    else
+      signal_config_file = argv[i];
+  }
+
+  // get encrypted key from Signal Desktop config
+  std::string encryptedkey = getEncryptedKey(signal_config_file);
+  if (encryptedkey.empty())
+  {
+    std::cout << "Failed to get encrypted key" << std::endl;
+    return 1;
+  }
+  if (g_verbose) [[unlikely]] std::cout << "(Encrypted key: " << encryptedkey << ")" << std::endl;
+
+  std::set<std::string> secrets;
+  std::string decrypted;
+
+  // get secret from libsecret (should work on Gnome and KDE 6)
+  getSecret_Mac(&secrets);
+  if (getKey(secrets, encryptedkey, decrypted)) // try what we got now (maybe we dont need to check kwallet)...
+  {
+    std::cout << " *** Decrypted key : " << decrypted << " ***" << std::endl;
+    return 0;
+  }
+
+  if (secrets.empty())
+  {
+    std::cout << "Failed to get any secrets" << std::endl;
+    return 1;
+  }
+
+  if (decrypted.empty())
+  {
+    std::cout << "Failed to decrypt valid key. :(" << std::endl;
+    return 1;
+  }
+
+  return 1;
+}
